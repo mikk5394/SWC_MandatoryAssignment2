92a3d8ba31762c14997e57248a01883b80faeced
hackerMike <mikkel_olsen@live.dk>
2018-11-29 13:43:21 +0100

    s
    s

--- src/main/java/com/example/demo/models/repositories/UserRepository.java
+++ src/main/java/com/example/demo/models/repositories/UserRepository.java
@@ -2,6 +2,6 @@ package com.example.demo.models.repositories;
 
 import com.example.demo.models.User;
 import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long>{
+//heajsdjasjdasjdsss
+public interface UserRepository extends CrudRepository<User, Long> {
 }
