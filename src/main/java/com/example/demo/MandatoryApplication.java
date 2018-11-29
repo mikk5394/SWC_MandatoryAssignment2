cd5f580f2074b673a8cccfcafb18fd70ce47ce31
Simo390m <31307612+Simo390m@users.noreply.github.com>
2018-11-29 13:04:19 +0100

    are we fixed?

--- src/main/java/com/example/demo/MandatoryApplication.java
+++ src/main/java/com/example/demo/MandatoryApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MandatoryApplication {
-
-
+    
     public static void main(String[] args) {
         SpringApplication.run(MandatoryApplication.class, args);
     }
