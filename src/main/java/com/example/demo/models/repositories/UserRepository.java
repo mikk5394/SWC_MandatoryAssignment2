4f2fe81fa9c812dff1f5f74c0fae870527e96d6a
hackerMike <mikkel_olsen@live.dk>
2018-11-29 13:36:43 +0100

    så der interfaces :))
    :+1:

--- src/main/java/com/example/demo/models/repositories/UserRepository.java
+++ src/main/java/com/example/demo/models/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.models.repositories;
 
-public class UserRepository {
+import com.example.demo.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
 }
