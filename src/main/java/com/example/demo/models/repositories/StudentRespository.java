9d38226af99ff3e04b6a78f51191fc8bca8ded59
hackerMike <mikkel_olsen@live.dk>
2018-11-29 13:48:46 +0100

    s
    s

--- src/main/java/com/example/demo/models/repositories/StudentRespository.java
+++ src/main/java/com/example/demo/models/repositories/StudentRespository.java
@@ -4,5 +4,5 @@ package com.example.demo.models.repositories;
 import com.example.demo.models.Student;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentRespository extends CrudRepository<Student, Long> {
+public interface StudentRespository extends CrudRepository<Student, Long>{
 }
