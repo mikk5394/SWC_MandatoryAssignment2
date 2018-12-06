5c513a00d8d81e951c166dd081a9de1e1b619b30
hackerMike <mikkel_olsen@live.dk>
2018-12-06 11:29:42 +0100

    lavet html for teacher frontpage og createCourse

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -1,4 +1,12 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StudentController {
+
+
 }
--- src/main/java/com/example/demo/controllers/TeacherController.java
+++ src/main/java/com/example/demo/controllers/TeacherController.java
@@ -1,4 +1,27 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TeacherController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/frontpage")
+    public String loggedIn(){
+
+        return "teacher/frontpage";
+    }
+
+
+    @GetMapping("/createCourse")
+    public String createCourse(){
+
+        return "teacher/createCourse";
+    }
+
 }
