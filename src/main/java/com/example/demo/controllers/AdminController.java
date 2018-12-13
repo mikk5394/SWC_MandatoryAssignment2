d974dcab350bd92f126b4005b6e932ce0e464b21
hackerMike <mikkel_olsen@live.dk>
2018-12-14 00:43:00 +0100

    done

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -2,22 +2,25 @@ package com.example.demo.controllers;
 
 import com.example.demo.Service.SessionHelper;
 import com.example.demo.models.Course;
+import com.example.demo.models.Student;
 import com.example.demo.models.repositories.CourseRepository;
+import com.example.demo.models.repositories.StudentRespository;
 import com.example.demo.models.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AdminController {
 
     @Autowired
-    UserRepository userRepository;
+    StudentRespository studentRespository;
     @Autowired
     CourseRepository courseRepository;
 
@@ -32,8 +35,8 @@ public class AdminController {
         }
     }
 
-    @GetMapping("/signedStudents")
-    public String signedStudents(HttpServletRequest request, Model model){
+    @GetMapping("/coursesWithList")
+    public String coursesWithList(HttpServletRequest request, Model model){
 
         if (SessionHelper.isAdmin(request)){
 
@@ -42,6 +45,39 @@ public class AdminController {
 
             List<Course> coursesWithWaitingList = new ArrayList<>();
 
+            for (int i = 0; i < courses.size(); i++){
+                if (!courses.get(i).getStudentWaitingList().isEmpty()){
+                    coursesWithWaitingList.add(courses.get(i));
+                }
+            }
+
+            model.addAttribute("course", coursesWithWaitingList);
+
+
+            return "adminDirectory/coursesWithList";
+        } else {
+            SessionHelper.logout(request);
+            return "redirect:/login";
+        }
+    }
+
+    @GetMapping("/signedStudents/{id}")
+    public String signedStudentList(@PathVariable Long id, HttpServletRequest request, Model model){
+
+        if (SessionHelper.isAdmin(request)){
+
+            Optional<Course> course = (courseRepository.findById(id));
+            Course c = course.get();
+
+
+            List<Student> studentsWaiting = new ArrayList<>();
+
+            for (int i = 0; i < c.getStudentWaitingList().size(); i++){
+                studentsWaiting.add(c.getStudentWaitingList().get(i));
+            }
+
+            model.addAttribute("student", studentsWaiting);
+            model.addAttribute("course", c);
 
 
             return "adminDirectory/signedStudents";
@@ -49,6 +85,22 @@ public class AdminController {
             SessionHelper.logout(request);
             return "redirect:/login";
         }
+    }
+
+    @PostMapping("/signedStudents")
+    public String signedStudentList(HttpServletRequest request, @ModelAttribute Course course, @ModelAttribute Student student){
+
+        if (SessionHelper.isAdmin(request)){
+        course.getStudentWaitingList().remove(student);
+        courseRepository.save(course);
+            return "redirect:/admin";
+        } else {
+            return "redirect:/login";
+        }
 
     }
+
+
+
+
 }
